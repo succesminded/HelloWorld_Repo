a6b0faf53c95e51201e7129d9b32c383bbf2075f
succesminded <succesminded@gmail.com>
2025-09-25 20:04:48 +0200

    init project

---
+++ Java_HelloWorld/src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//3. homework
+		System.out.println(5+3);
+		//4. homework
+		System.out.println(3-5);
+		//5. homework
+		System.out.println(5*3);
+		//7. homework
+		System.out.println(50/10/5);
+		//8. homework
+		System.err.println("Hello World");
+		//9. homework
+		System.out.println(5>3);
+		System.out.println(3>3);
+		System.out.println(3>=3);
+		System.out.println(1<2);
+		System.out.println(8<=4);
+		System.out.println(0!=15);
+		System.out.println(9==9);
+		System.out.println(5==7);
+		//10. homework
+		System.out.println(Math.PI);
+	}
+}
